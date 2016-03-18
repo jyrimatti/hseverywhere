0b2aa3bf1a3d37f878fb54158c1768ce9209b6a7
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-03-19 00:45:30 +0200

    include default.nix

---
+++ default.nix
@@ -0,0 +1,31 @@
+{ nixpkgs ? import <nixpkgs> {}, compiler ? "ghcjs" }:
+
+let
+
+  inherit (nixpkgs) pkgs;
+
+  f = { mkDerivation, base, deepseq, ghcjs-base, react-flux, stdenv
+      , text, time, transformers
+      }:
+      mkDerivation {
+        pname = "hsEverywhere";
+        version = "0.1.0.0";
+        src = ./.;
+        isLibrary = false;
+        isExecutable = true;
+        executableHaskellDepends = [
+          base deepseq ghcjs-base react-flux text time transformers
+        ];
+        buildDepends = [pkgs.haskellPackages.cabal-install];
+        license = stdenv.lib.licenses.mit;
+      };
+
+  haskellPackages = if compiler == "default"
+                       then pkgs.haskellPackages
+                       else pkgs.haskell.packages.${compiler};
+
+  drv = haskellPackages.callPackage f {};
+
+in
+
+  if pkgs.lib.inNixShell then drv.env else drv
