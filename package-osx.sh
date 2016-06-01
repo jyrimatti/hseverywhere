dbfe583aa9ca1fe95913b1c0fc648a45c06b05e0
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-01 22:33:49 +0300

    semi-purified package-osx.sh

--- package-osx.sh
+++ package-osx.sh
@@ -1,17 +1,20 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
+
+### Note! Note pure, requires xcodebuild and hdiutil! ###
 
 app=$(basename $PWD)
 
 nix-shell --run "cabal clean && cabal build $app"
-cp -f dist/build/$app/$app.jsexe/all.js $app/
 
 cp -f dist/build/$app/$app.jsexe/all.js $app/
 cp -fR files/* $app/
 mv $app/rn-cli.config.desktop.js $app/rn-cli.config.js
 
 dir=$PWD
-nix-shell -p nodejs-5_x --run "cd $app/osx && xcodebuild -scheme $app -configuration Release -target $app archive -archivePath $dir/dist/$app.xcarchive"
-hdiutil create -volname $app -srcfolder dist/$app.xcarchive/Products/Applications/$app.app -ov -format UDZO dist/$app.dmg
+nix-shell -p nodejs-5_x --run "cd $app/osx && /usr/bin/xcodebuild -scheme $app -configuration Release -target $app archive -archivePath $dir/dist/$app.xcarchive"
+/usr/bin/hdiutil create -volname $app -srcfolder dist/$app.xcarchive/Products/Applications/$app.app -ov -format UDZO dist/$app.dmg
 
 echo "Generated dist/$app.dmg"
