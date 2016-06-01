23b6f7c4436e56465fae7562df27643bd8cb57db
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-01 21:52:44 +0300

    purified package-android.sh

--- package-android.sh
+++ package-android.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
 
 app=$(basename $PWD)
 
@@ -11,7 +13,7 @@ mv $app/rn-cli.config.others.js $app/rn-cli.config.js
 
 # https://github.com/facebook/react-native/issues/5615
 nix-shell -p nodejs-5_x --run "cd $app && mkdir -p android/app/build/intermediates/assets/release && node ./node_modules/react-native-cli/index.js bundle --platform android --dev false --entry-file index.android.js --bundle-output android/app/build/intermediates/assets/release/index.android.bundle --assets-dest android/app/src/main/res/"
-sh -c "cd $app/android; ./gradlew assembleRelease"
+nix-shell -p jre8 androidsdk which --run "cd $app/android; ANDROID_HOME=\$(which android | xargs dirname | xargs dirname)/libexec/android-sdk-macosx ./gradlew --no-daemon assembleRelease"
 
 cp $app/android/app/build/outputs/apk/app-release.apk dist/$app.apk
 echo "Generated dist/$app.apk"
