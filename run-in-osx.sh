e22e929ddff474c6788c0ad7743cc2f50319e5f4
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-02 00:12:28 +0300

    "purified" run-in-osx.sh

--- run-in-osx.sh
+++ run-in-osx.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
+
+### Note! Note pure, requires xcodebuild! ###
 
 port=${1:-8083}
 
@@ -9,7 +13,7 @@ cp -f dist/build/$app/$app.jsexe/all.js $app/
 cp -fR files/* $app/
 mv $app/rn-cli.config.desktop.js $app/rn-cli.config.js
 
-sed -i '' "s/8081/$port/g" $app/osx/$app/AppDelegate.m
+nix-shell --pure --run "sed -i \"s/8081/$port/g\" $app/osx/$app/AppDelegate.m"
 
-nix-shell -p nodejs-5_x --run "cd $app && open osx/$app.xcodeproj"
-nix-shell -p nodejs-5_x --run "cd $app && PORT=$port npm run start-osx"
+nix-shell -p nodejs-5_x xdg_utils --run "cd $app && xdg-open osx/$app.xcodeproj"
+nix-shell --pure -p nodejs-5_x --run "cd $app && PORT=$port npm run start-osx"
