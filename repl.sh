720a379b4f07f587549b13f70cc998f97ea05522
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-01 22:42:20 +0300

    purified repl.sh

--- repl.sh
+++ repl.sh
@@ -1,4 +1,7 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i bash -p nix bash
+set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
 
 port=${1:-8080}
 
