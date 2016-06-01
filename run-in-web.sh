02224a6202fc6e642a0537fc4a59f74ab3468ef7
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-02 00:08:46 +0300

    purified run-in-web.sh

--- run-in-web.sh
+++ run-in-web.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
 
 port=${1:-8084}
 
@@ -9,7 +11,7 @@ cp -f dist/build/$app/$app.jsexe/all.js $app/
 cp -fR files/* $app/
 mv $app/rn-cli.config.others.js $app/rn-cli.config.js
 
-open http://localhost:$port/
+nix-shell -p xdg_utils --run "xdg-open http://localhost:$port/"
 
-nix-shell -p nodejs-5_x --run "cd $app && PORT=$port npm run start-web"
+nix-shell --pure -p nodejs-5_x --run "cd $app && PORT=$port npm run start-web"
 
