76b98e1168f439d750255ca0b1d9292f9cb6d5be
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-02 00:22:13 +0300

    "purified" run-in-ios.sh

--- run-in-ios.sh
+++ run-in-ios.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
+
+### Note! Note pure, requires xcode! ###
 
 port=${1:-8082}
 
@@ -9,7 +13,8 @@ cp -f dist/build/$app/$app.jsexe/all.js $app/
 cp -fR files/* $app/
 mv $app/rn-cli.config.others.js $app/rn-cli.config.js
 
-sed -i '' "s/8081/$port/g" $app/ios/$app/AppDelegate.m
+nix-shell --pure --run "sed -i \"s/8081/$port/g\" $app/ios/$app/AppDelegate.m"
 
 nix-shell -p nodejs-5_x --run "cd $app && node ./node_modules/react-native-cli/index.js run-ios" &
-nix-shell -p nodejs-5_x --run "cd $app && PORT=$port npm run start --loglevel verbose"
+sleep 1
+nix-shell --pure -p nodejs-5_x --run "cd $app && PORT=$port npm run start --loglevel verbose"
