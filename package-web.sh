1bb95c025835864cba279e46325bc899c5bb980c
Jyri-Matti Lähteenmäki <jyri-matti.lahteenmaki@solita.fi>
2016-06-01 21:55:58 +0300

    purified package-web.sh

--- package-web.sh
+++ package-web.sh
@@ -1,5 +1,7 @@
-#!/bin/sh
+#! /usr/bin/env nix-shell
+#! nix-shell --pure -i bash -p nix bash
 set -eu
+export NIX_PATH=nixpkgs=https://github.com/NixOS/nixpkgs-channels/archive/$(cat nix-revision.txt).tar.gz
 
 app=$(basename $PWD)
 
